8eade754a5a673f0fbc1b1d27813292ff64ce501
Mark Nunnikhoven <me@markn.ca>
2016-03-26 14:28:02 -0400

    Implemented filtering on Computers.get()

--- deepsecurity/computers.py
+++ deepsecurity/computers.py
@@ -13,21 +13,81 @@ class Computers(core.CoreDict):
     self.manager = manager
     self.log = self.manager.log if self.manager else None
 
-  def get(self):
-    # get with no arguments = hostRetrieve() with ALL_HOSTS
-    call = self.manager._get_request_format(call='hostDetailRetrieve')
-    call['data'] = {
-        'hostFilter': {
-          'hostGroupID': None,
-          'hostID': None,
-          'securityProfileID': None,
-          'type': 'ALL_HOSTS',
-        },
-        'hostDetailLevel': 'HIGH'
-      }
+  def get(self, detail_level='HIGH', computer_id=None, computer_group_id=None, security_profile_id=None, computer_name=None, external_id=None, external_group_id=None):
+    """
+    Get all or a filtered set of computers from Deep Security
+
+    Can filter by:
+      computer_id
+      computer_group_id
+      security_profile_id
+      computer_name (specific or starts with using name*)
+      external_id
+      external_group_id
+
+    If multiple filters are requested, only one is applied. The priority is;
+      external_id
+      external_group_id
+      computer_name
+      computer_id
+      computer_group_id
+      security_profile_id
+
+    detail_level can be set to one of ['HIGH', 'MEDIUM', 'LOW']
+    """
+    # make sure we have a valid detail level
+    detail_level = detail_level.upper()
+    if not detail_level in ['HIGH', 'MEDIUM', 'LOW']: detail_level = 'HIGH'
+
+    call = None
+    if external_id and external_group_id:
+      call = self.manager._get_request_format(call='hostDetailRetrieveByExternal')
+      if external_id:
+        call['data'] = {
+          'externalFilter': {
+            'hostExternalID': external_id,
+            'hostGroupExternalID': None,
+          },
+          'hostDetailLevel': detail_level
+        }
+      elif external_group_id:
+        call['data'] = {
+          'externalFilter': {
+            'hostExternalID': None,
+            'hostGroupExternalID': external_group_id,
+          },
+          'hostDetailLevel': detail_level
+        }
+    if computer_name:
+      if computer_name.endswith('*'):
+        call = self.manager._get_request_format(call='hostDetailRetrieveByNameStartsWith')
+        call['data'] = {
+          'hostname': computer_name,
+          'hostDetailLevel': detail_level
+          }
+      else:
+        call = self.manager._get_request_format(call='hostDetailRetrieveByName')
+        call['data'] = {
+          'startsWithHostname': computer_name.rstrip('*'),
+          'hostDetailLevel': detail_level
+          }
+    else:
+      # get with no arguments = hostRetrieve() with ALL_HOSTS
+      call = self.manager._get_request_format(call='hostDetailRetrieve')
+      call['data'] = {
+          'hostFilter': {
+            'hostGroupID': None,
+            'hostID': None,
+            'securityProfileID': None,
+            'type': 'ALL_HOSTS',
+          },
+          'hostDetailLevel': detail_level
+        }
+
     response = self.manager._request(call)
     
     if response and response['status'] == 200:
+      if not type(response['data']) == type([]): response['data'] = [response['data']]
       for computer in response['data']:
         computer_obj = Computer(computer, self.log)
         if computer_obj:
@@ -46,6 +106,14 @@ class ComputerGroups(core.CoreDict):
     self.log = self.manager.log if self.manager else None
 
   def get(self, name=None, group_id=None):
+    """
+    Get all or a filtered set of computer groups from Deep Security
+
+    If a name or group_id is specified, will only retrieve the 
+    computer groups matching the name or group_id.
+
+    If both are specified, name takes priority
+    """
     call = None
     if name or group_id:
       # filtered call
